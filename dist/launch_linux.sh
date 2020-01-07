f5a7f1ef7cc45521583a0a5810c6474663f3864b
miloas <0x9357@gmail.com>
2020-01-07 12:30:27 +0800

    memory limit

--- dist/launch_linux.sh
+++ dist/launch_linux.sh
@@ -8,6 +8,11 @@ JLINK_VM_OPTIONS="\
 --add-exports jdk.compiler/com.sun.tools.javac.model=ALL-UNNAMED \
 --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
 --add-opens jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED"
+
+JVM_OPTIONS="\
+-Xmx512M \
+-XX:+ExitOnOutOfMemoryError"
+
 DIR=`dirname $0`
 CLASSPATH_OPTIONS="-classpath $DIR/classpath/gson-2.8.5.jar:$DIR/classpath/protobuf-java-3.9.1.jar:$DIR/classpath/java-language-server.jar"
-$DIR/linux/bin/java $JLINK_VM_OPTIONS $CLASSPATH_OPTIONS $@
+$DIR/linux/bin/java $JLINK_VM_OPTIONS $JVM_OPTIONS $CLASSPATH_OPTIONS $@
